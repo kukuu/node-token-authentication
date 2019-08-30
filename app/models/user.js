8f0419ccc847bbb5bc0b3c405dede4df3d59ffa0
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2019-08-30 10:35:04 +0100

    Updated model.

--- app/models/user.js
+++ app/models/user.js
@@ -2,6 +2,9 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 // set up a mongoose model
+//mongoose.model takes 2 paameters: an identification. This will be pluralised by mongodb for the 
+//collection name automatically as 'users'
+//second parameter is an instantiated schema from its object created
 module.exports = mongoose.model('User', new Schema({ 
 	name: String, 
 	password: String, 
